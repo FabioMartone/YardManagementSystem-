53180fd5bc24fd4df9888b909aed702176b0e5a7
fabio <fabio@192.168.1.26>
2021-07-04 18:26:01 +0200

    Allineamento

---
+++ YardManagementSystem--main/src/main/java/com/sad/yardmanagementsystem/controller/UserHomepageCorriereController.java
@@ -0,0 +1,32 @@
+package com.sad.yardmanagementsystem.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sad.yardmanagementsystem.service.UtenteService;
+
+
+@Controller
+@RequestMapping("/home_corriere")
+public class UserHomepageCorriereController {
+
+	private UtenteService userService;
+
+	public UserHomepageCorriereController(UtenteService userService) {
+		super();
+		this.userService = userService;
+	}
+		
+	@GetMapping
+	public String showHomepage() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null && auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("CORRIERE"))) {
+			return "home_corriere";
+		}
+		else return "redirect:/home_gestore";
+	}
+	
+}
